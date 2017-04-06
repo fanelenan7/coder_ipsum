4c662150f28c678c5dfc9f023234c64b845e44e3
Elena Santana <fanelenan7@gmail.com>
2017-04-06 16:19:57 -0400

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './components/Home/Home.js';
-import './index.css';
-
-ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
-);
